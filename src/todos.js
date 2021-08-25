975dd2bff99f3d535ab7f01047de92dc59a4f5a8
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-08-26 03:22:39 +0430

    feat: create todo.js functional for webpack

--- src/todos.js
+++ src/todos.js
@@ -1,27 +1,78 @@
+import { uuidv4 } from "uuid/v4";
 // Setup the empty todos array
+const toDoList = [];
 
 // loadTodos
 // Arguments: none
 // Return value: none
+const loadTasks = () => {
+    const toDoList = localStorage.getItem("toDoList");
 
+    try {
+        toDoList ? JSON.parse(toDoListJSON) : [];
+    } catch (e) {
+        [];
+    }
+};
+
+loadTasks();
 // saveTodos
 // Arguments: none
 // Return value: none
+const saveTask = () => {
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));
+};
 
 // getTodos
 // Arguments: none
 // Return value: todos array
+const getToDoList = () => toDoList;
 
 // createTodo
 // Arguments: todo text
 // Return value: none
+const createToDoList = (toDoText) => {
+    const trimText = toDoText.trim();
+    if (trimText) {
+        toDoList.push({
+            id: uuidv4(),
+            text: trimText,
+            completed: false,
+        });
+        saveTask();
+        renderToDo();
+    }
+};
 
 // removeTodo
 // Arguments: id of todo to remove
 // Return value: none
+const removeTask = (id) => {
+    const toDoIndex = toDoList.findIndex((toDo) => toDo.id === id);
+    if (toDoIndex > -1) {
+        toDoList.splice(toDoIndex, 1);
+        saveTask();
+    }
+};
 
 // toggleTodo
 // Arguments: id of todo to toggle
 // Return value: none
+const toggleTask = (id) => {
+    const task = toDoList.find((toDo) => toDo.id === id);
+
+    if (task) {
+        task.completed = !task.completed;
+        saveTask();
+    }
+};
 
 // Make sure to call loadTodos and setup the exports
+export {
+    toggleTask,
+    removeTask,
+    createToDoList,
+    loadTasks,
+    getToDoList,
+    saveTask,
+};
