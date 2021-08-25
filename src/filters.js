8606e4b0070972fa7e82bc3f8cc4e73e9d45cb62
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-08-26 03:01:22 +0430

    feat: import filter.js to project

--- src/filters.js
+++ src/filters.js
@@ -1,11 +1,32 @@
 // Set up filters default object
 
+const filters = {
+    searchText: "",
+    hideCompleted: false,
+};
+
+const getFilters = () => filters;
+
 // getFilters
 // Arguments: none
 // Return value: filters object
+
+const setFilter = (updates) => {
+    const searchFilter = updates.searchText;
+    const hideTask = updates.hideCompleted;
+    if (searchFilter) {
+        filters.searchText = searchFilter;
+    }
+
+    if (hideTask) {
+        filters.hideCompleted = hideTask;
+    }
+    renderToDo();
+};
 
 // setFilters
 // Arguments: updates object with optional searchText or hideCompleted
 // Return value: none
 
 // Make sure to set up the exports
+export { getFilters, setFilter };
